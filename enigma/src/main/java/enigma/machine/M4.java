8714c4828786f039e6791d639e4d7ede589f9ac4
Freitas Pepeu <pedrosteamgtav@gmail.com>
2023-05-09 09:32:03 -0300

    started to plan m4 machine class

--- enigma/src/main/java/enigma/machine/M4.java
+++ enigma/src/main/java/enigma/machine/M4.java
@@ -1,6 +1,33 @@
 public class M4 {
 
     RotorsAssociation rotorassociation = new RotorsAssociation();
+    Reflector reflector = new Reflector();
+    Scramble scramble = new Scramble();
+    Scanner sc = new Scanner(System.in);
+
+    // configure scramble
     
+
+    // add the phrase
+    String phrase = sc.nextLine();
+
+    for(int i = 0; i < phrase.length(); i++) {
+        // pass through rotor
+    
+        // hits reflector
+    
+        // pass through rotation again
+        
+        // rotate the rotors
+        
+        // go to scramble
+        
+        // comeback as another letter
+    
+        // print
+
+    }
+
+    sc.close();
     
 }
